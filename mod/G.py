cf6c0b90c38baaad73b66b795163290cd560f1e7
Saluton874 <105183174+Saluton874@users.noreply.github.com>
2022-10-25 11:22:53 +0900

    グローバル変数入れ

---
+++ mod/G.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+''' グローバル変数 '''
+
+import os
+
+path = os.path.abspath(os.path.dirname(__file__))+'/'
+neologd = '-d /usr/local/lib/mecab/dic/mecab-ipadic-neologd/ '
